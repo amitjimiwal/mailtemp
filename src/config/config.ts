508e7a2be09c723362446a2249a6276e7506bec1
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-02-14 23:20:21 +0530

    Remove unused environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,10 +9,6 @@ const config={
      databaseID:String(process.env.VITE_APPWRITE_DATABASE_ID),
      userReadCollectionID:String(process.env.VITE_APPWRITE_USER_READ_COLLECTION_ID),
      userCollectionID:String(process.env.VITE_APPWRITE_USER_COLLECTION_ID),
-     bucketID:String(process.env.VITE_APPWRITE_BUCKET_ID),
-     socialsCollectionID:String(process.env.VITE_APPWRITE_SOCIALS_COLLECTION_ID),
-     imagePreviewBaseURL:String(process.env.VITE_IMAGEPREVIEW_ENDPOINT),
-     frontendURL:String(process.env.VITE_FRONTEND_ENDPOINT),
      appwriteAPIKey:String(process.env.VITE_APPWRITE_API_KEY),
 }
 export default config;
